78712a2807599d35b725e6f3df52632ed7242960
moto4321 <moto432195@gmail.com>
2025-01-27 17:13:08 +0900

    feat: user update api 개발중

---
+++ src/main/java/com/longing/longing/user/controller/port/UserService.java
@@ -0,0 +1,4 @@
+package com.longing.longing.user.controller.port;
+
+public interface UserService {
+}
---
+++ src/main/java/com/longing/longing/user/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.longing.longing.user.service;
+
+import com.longing.longing.user.controller.port.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
