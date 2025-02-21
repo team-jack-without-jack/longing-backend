b9a272687910a49271e167c31a5d085c1ab87b43
moto4321 <moto432195@gmail.com>
2025-02-21 23:52:29 +0900

    fix: RestTemplate bean

--- src/main/java/com/longing/longing/LongingApplication.java
+++ src/main/java/com/longing/longing/LongingApplication.java
@@ -2,7 +2,9 @@ package com.longing.longing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication(
@@ -16,6 +18,11 @@ public class LongingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LongingApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
