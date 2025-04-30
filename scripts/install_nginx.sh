a283b8b48d26e233f3ff75fc2ac7f55277ce2231
moto4321 <moto432195@gmail.com>
2025-04-30 13:02:39 +0900

    fix: nginx install sh fix

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
+set -x
 
-yum install -y nginx
+# 1) amazon-linux-extras에 nginx1 토픽 활성화
+if ! yum list installed nginx &>/dev/null; then
+  amazon-linux-extras enable nginx1
+  yum clean metadata
+  yum install -y nginx
+fi
+
+# 2) 서비스 등록 및 시작
 systemctl enable nginx
 systemctl start nginx
+
+# 3) (선택) 상태 확인
+systemctl status nginx
