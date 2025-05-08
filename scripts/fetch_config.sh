85dc61be1faedd0f5b9949b5999f3639108d5890
moto4321 <moto432195@gmail.com>
2025-05-08 12:46:17 +0900

    feat: kms

--- scripts/fetch_config.sh
+++ scripts/fetch_config.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
-set -e
-
-BUCKET=longing-op-config
-KEY=application-prod.yml
-DEST=/home/ec2-user/app/config
-
-# (1) awscli 설치 확인 (없으면 설치)
-command -v aws >/dev/null || yum install -y awscli
-
-# (2) 디렉터리 준비
-mkdir -p $DEST
-
-# (3) S3에서 내려받기
-aws s3 cp s3://$BUCKET/$KEY $DEST/$KEY
-
-# (4) 소유권/퍼미션
-chown ec2-user:ec2-user $DEST/$KEY
-chmod 600 $DEST/$KEY
+##!/bin/bash
+#set -e
+#
+#BUCKET=longing-op-config
+#KEY=application-prod.yml
+#DEST=/home/ec2-user/app/config
+#
+## (1) awscli 설치 확인 (없으면 설치)
+#command -v aws >/dev/null || yum install -y awscli
+#
+## (2) 디렉터리 준비
+#mkdir -p $DEST
+#
+## (3) S3에서 내려받기
+#aws s3 cp s3://$BUCKET/$KEY $DEST/$KEY
+#
+## (4) 소유권/퍼미션
+#chown ec2-user:ec2-user $DEST/$KEY
+#chmod 600 $DEST/$KEY
