0cf213e5048867138c534f7f368c0269076fcfb1
moto4321 <moto432195@gmail.com>
2025-05-25 18:59:43 +0900

    feat: test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,9 @@ set -e
 set -x
 
 LOG_KEY=logback-spring.xml
+APP_DIR=/home/ec2-user/app
+CONFIG_DIR=$APP_DIR/config
+JAR_NAME=longing-0.0.1-SNAPSHOT.jar
 
 # (1) JAVA_HOME 설정
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
@@ -17,11 +20,11 @@ cd /home/ec2-user/app
 #  > app.out 2>&1 &
 
 nohup $JAVA_HOME/bin/java \
-  -Dlogging.config=/home/ec2-user/app/config/logback-spring.xml \
-  -jar /home/ec2-user/app/longing-0.0.1-SNAPSHOT.jar \
+  -Dlogging.config=file:$CONFIG_DIR/logback-spring.xml \
+  -jar $APP_DIR/$JAR_NAME \
   --spring.profiles.active=prod \
-  --spring.config.additional-location=classpath:/,file:/home/ec2-user/app/config/ \
-  > /home/ec2-user/app/app.out 2>&1 &
+  --spring.config.additional-location=classpath:/,file:$CONFIG_DIR/ \
+  > $APP_DIR/app.out 2>&1 &
 
 # (4) 바로 실행 확인
 sleep 5
