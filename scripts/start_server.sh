9f71efd8c5942c6ba8c29aed4d9acbb0064735c5
moto4321 <moto432195@gmail.com>
2025-05-25 18:49:10 +0900

    feat: start server fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,8 @@
 set -e
 set -x
 
+LOG_KEY=logback-spring.xml
+
 # (1) JAVA_HOME 설정
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 export PATH=$JAVA_HOME/bin:$PATH
@@ -10,9 +12,16 @@ export PATH=$JAVA_HOME/bin:$PATH
 cd /home/ec2-user/app
 
 # (3) 백그라운드로 실행 & 로그
-nohup $JAVA_HOME/bin/java -jar longing-0.0.1-SNAPSHOT.jar \
+#nohup $JAVA_HOME/bin/java -jar longing-0.0.1-SNAPSHOT.jar \
+#  --spring.profiles.active=prod \
+#  > app.out 2>&1 &
+
+nohup $JAVA_HOME/bin/java \
+  -Dlogging.config=/home/ec2-user/app/config/logback-spring.xml \
+  -jar /home/ec2-user/app/longing-0.0.1-SNAPSHOT.jar \
   --spring.profiles.active=prod \
-  > app.out 2>&1 &
+  --spring.config.additional-location=classpath:/,file:/home/ec2-user/app/config/ \
+  > /home/ec2-user/app/app.out 2>&1 &
 
 # (4) 바로 실행 확인
 sleep 5
