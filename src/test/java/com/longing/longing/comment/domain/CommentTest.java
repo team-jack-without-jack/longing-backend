8a0ed281445c5f4a332b8af4325ac5f6b2eb81a6
moto4321 <moto432195@gmail.com>
2025-06-03 21:23:22 +0900

    feat: comment test

---
+++ src/test/java/com/longing/longing/comment/domain/CommentTest.java
@@ -0,0 +1,41 @@
+package com.longing.longing.comment.domain;
+
+import com.longing.longing.post.domain.Post;
+import com.longing.longing.user.domain.User;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CommentTest {
+
+    @Test
+    public void CommentCreate으로_댓글을_만들_수_있다() {
+        // given
+        CommentCreate commentCreate = CommentCreate.builder()
+                .postId(1L)
+                .content("test content")
+                .build();
+
+        User user = User.builder()
+                .build();
+
+        Post post = Post.builder()
+                .id(1L)
+                .title("test title")
+                .content("test content")
+                .build();
+
+        // when
+        Comment comment = Comment.from(user, post, commentCreate);
+
+        // then
+        assertThat(comment.getContent()).isEqualTo("test content");
+        assertThat(comment.getPost().getTitle()).isEqualTo("test title");
+        assertThat(comment.getPost().getContent()).isEqualTo("test content");
+    }
+
+    @Test
+    public void CommentUpdate로_댓글을_수정할_수_있다() {
+
+    }
+}
