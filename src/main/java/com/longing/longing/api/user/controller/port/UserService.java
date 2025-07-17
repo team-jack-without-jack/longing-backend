80d4369a9caaf7d55802b7fc2a30ae4c722eabc1
moto4321 <moto432195@gmail.com>
2025-07-17 21:57:19 +0900

    feat: 블락된 유저 목록 조회 api

--- src/main/java/com/longing/longing/api/user/controller/port/UserService.java
+++ src/main/java/com/longing/longing/api/user/controller/port/UserService.java
@@ -2,8 +2,8 @@ package com.longing.longing.api.user.controller.port;
 
 import com.longing.longing.api.user.Provider;
 import com.longing.longing.api.user.domain.UserUpdate;
-import com.longing.longing.config.auth.dto.CustomUserDetails;
 import com.longing.longing.api.user.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
@@ -16,4 +16,6 @@ public interface UserService {
     void blockUser(User user, long blockUserId);
 
     void cancelBlockUser(User user, long blockUserId);
+
+    Page<User> getBlockedUserList(User user, String keyword, int page, int size, String sortBy, String sortDirection);
 }
--- src/main/java/com/longing/longing/api/user/service/port/UserRepository.java
+++ src/main/java/com/longing/longing/api/user/service/port/UserRepository.java
@@ -3,7 +3,8 @@ package com.longing.longing.api.user.service.port;
 import com.longing.longing.api.user.Provider;
 import com.longing.longing.api.user.domain.User;
 import com.longing.longing.api.user.domain.UserBlock;
-import com.longing.longing.api.user.infrastructure.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -28,6 +29,8 @@ public interface UserRepository {
     Optional<UserBlock> findBlockedData(User user, User blockedUser);
 
     void deleteByUserBlockedId(long userBlockId);
+
+    Page<User> findBlockedUserList(Long id, String keyword, Pageable pageable);
 }
 
 
